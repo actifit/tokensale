554627f7793468206ef9a8af20e60247c10ed7b9
Mohammad <mcfarhat@gmail.com>
2021-07-05 13:18:23 +0300

    Implement buy functionality

--- src/component/Invest/index.js
+++ src/component/Invest/index.js
@@ -7,6 +7,9 @@ import Fade from 'react-reveal/Fade';
 import './style.css';
 import Navbars from '../../layout/Navbars';
 let glbprops;
+let bnbAfit = 0, bnbAfitx = 0;
+let buyAfitOn = false;
+let buyAfitxOn = false;
 
 function claimAFITRewards(){
 	console.log('claiming AFIT');
@@ -25,7 +28,40 @@ function claimAFITXRewards(){
 		console.log(res);
 	});
 }
-//console.log('ETHHHH');
+
+function buyAfitConfirm(props){
+	let cancelAction = false;
+	if (bnbAfit < props.data.minBnBAFIT){ bnbAfit = props.data.minBnBAFIT;cancelAction = true; 
+	}
+	if (bnbAfit > props.data.maxBnBAFIT){ bnbAfit = props.data.maxBnBAFIT;;cancelAction = true;
+	}	
+	if (cancelAction){
+		return;
+	}else{
+		//console.log(window.web3.toWei(bnbAfit, "ether"));
+		glbprops.data.contractAFIT.methods.deposit().send({from: glbprops.data.account, value: glbprops.data.web3.utils.toWei(bnbAfit, "ether")}).then(function(res){
+			console.log(res);
+		});
+	}
+}
+
+function buyAfitxConfirm(props){
+	let cancelAction = false;
+	console.log(bnbAfitx);
+	if (bnbAfitx < props.data.minBnBAFITX){ bnbAfitx = props.data.minBnBAFITX;cancelAction = true; 
+	}
+	if (bnbAfitx > props.data.maxBnBAFITX){ bnbAfitx = props.data.maxBnBAFITX;cancelAction = true;
+	}	
+	if (cancelAction){
+		return;
+	}else{
+		console.log(bnbAfitx);
+		//console.log(window.web3.toWei(bnbAfitx, "ether"));
+		glbprops.data.contractAFITX.methods.deposit().send({from: glbprops.data.account, value: glbprops.data.web3.utils.toWei(bnbAfitx, "ether")}).then(function(res){
+			console.log(res);
+		});
+	}
+}
 //console.log(window.ethereum);
 
 export default function Landing(props) {
@@ -68,12 +104,34 @@ export default function Landing(props) {
                         <div className="u-invest-item-list"> <img src="/assets/img/list.svg" />&nbsp;&nbsp;Increase User Rank</div>
                         <div className="u-invest-item-list"> <img src="/assets/img/list.svg" />&nbsp;&nbsp;Core DeFi token </div>
                         <div className="u-invest-item-list"> <img src="/assets/img/list.svg" />&nbsp;&nbsp;Yield Farms, Pools & Vaults (Workouts & Shakes) </div>
-                        <div className="u-flex-around invest-btn u-btn u-responsive-after">BUY AFITX </div>
+                        <div className="u-flex-around invest-btn u-btn u-responsive-after" onClick={() => {buyAfitxOn = true;}}>BUY AFITX </div>
 						
-						<div>{props.data && !isNaN(props.data.investedBnBAFITX) && props.data.investedBnBAFITX > 0 ? ( <div className="u-flex-around invest-btn u-responsive-after">Invested BNB {props.data.investedBnBAFITX}</div>):(<span />)}</div>
+						{buyAfitxOn?
+							(
+							<div>
+								<label className="u-flex-around invest-btn u-btn u-responsive-after">
+								BNB:
+								<input type="number" step="0.001" className="actifit-input" min={props.data.minBnBAFITX} max={props.data.maxBnBAFITX} onChange={(evt) => { 
+									if (evt.target.value < 0) evt.target.value = props.data.minBnBAFITX; 
+									if (evt.target.value > props.data.maxBnBAFITX) evt.target.value = props.data.maxBnBAFITX; 
+									bnbAfitx = evt.target.value; 
+									
+									}} 
+									
+								onBlur={(evt) => {
+									if (evt.target.value < props.data.minBnBAFITX) evt.target.value = props.data.minBnBAFITX; 
+									if (evt.target.value > props.data.maxBnBAFITX) evt.target.value = props.data.maxBnBAFITX; 
+									bnbAfitx = evt.target.value; 
+								}}	
+								/></label>
+								<div className="u-flex-around invest-btn u-btn u-responsive-after" onClick={() => {buyAfitxConfirm(props)}}>Proceed</div>
+							</div>
+							):(<span></span>)
+						}
+						<div>{props.data && !isNaN(props.data.investedBnBAFITX) && props.data.investedBnBAFITX > 0 ? ( <div className="claimable claimable-hidden">Invested BNB {props.data.investedBnBAFITX}</div>):(<span />)}</div>
 						<div>{props.data && !isNaN(props.data.claimableAFITX) && props.data.claimableAFITX > 0 ? ( 
 							<div>
-								<div className="u-flex-around invest-btn u-responsive-after">Claimable AFITX {props.data.claimableAFITX}</div>
+								<div className="claimable claimable-hidden">Claimable AFITX {props.data.claimableAFITX}</div>
 								<div>
 									{props.data.fundsClaimableAFITX && ! props.data.claimedAFITX ? (
 										<div className="u-flex-around invest-btn u-btn u-responsive-after" onClick={() => {claimAFITXRewards()}}>CLAIM AFITX</div>
@@ -82,7 +140,7 @@ export default function Landing(props) {
 								</div>
 							</div>
 						):(<span />)}</div>
-						<div>{props.data && props.data.fundsClaimableAFITX && props.data.claimedAFITX ? (<div className="u-flex-around invest-btn u-responsive-after">AFITX Claimed!</div>):(<span />)}</div>
+						<div>{props.data && props.data.fundsClaimableAFITX && props.data.claimedAFITX ? (<div className="claimable claimable-hidden">AFITX Claimed!</div>):(<span />)}</div>
 						
                     </div>
                 </Fade>
@@ -102,12 +160,36 @@ export default function Landing(props) {
                         <div className="u-invest-item-list"> <img src="/assets/img/list.svg" />&nbsp;&nbsp;Increase User Rank</div>
                         <div className="u-invest-item-list"> <img src="/assets/img/list.svg" />&nbsp;&nbsp;Reward friends for tracking fitness activity</div>
                         <div className="u-invest-item-list"> <img src="/assets/img/list.svg" />&nbsp;&nbsp;Yield Farms, Pools & Vaults (Workouts & Shakes) </div>
-                        <div className="u-flex-around invest-btn u-btn u-responsive-after">BUY AFIT</div>
+                        <div className="u-flex-around invest-btn u-btn u-responsive-after" onClick={() => {buyAfitOn = true; }}>BUY AFIT </div>
 						
-						<div>{props.data && !isNaN(props.data.investedBnBAFIT) && props.data.investedBnBAFIT > 0 ? ( <div className="u-flex-around invest-btn u-responsive-after">Invested BNB {props.data.investedBnBAFIT}</div>):(<span />)}</div>
+						{buyAfitOn?
+							(
+							<div>
+								<label className="u-flex-around invest-btn u-btn u-responsive-after">
+								BNB:
+								<input type="number" step="0.001" className="actifit-input" min={props.data.minBnBAFIT} max={props.data.maxBnBAFIT} onChange={(evt) => { 
+									if (evt.target.value < 0) evt.target.value = props.data.minBnBAFIT; 
+									if (evt.target.value > props.data.maxBnBAFIT) evt.target.value = props.data.maxBnBAFIT; 
+									bnbAfit = evt.target.value; 
+									
+									}} 
+									
+								onBlur={(evt) => {
+									if (evt.target.value < props.data.minBnBAFIT) evt.target.value = props.data.minBnBAFIT; 
+									if (evt.target.value > props.data.maxBnBAFIT) evt.target.value = props.data.maxBnBAFIT; 
+									bnbAfit = evt.target.value; 
+								}}	
+								/></label>
+								<div className="u-flex-around invest-btn u-btn u-responsive-after" onClick={() => {buyAfitConfirm(props)}}>Proceed</div>
+							</div>
+							):(<span></span>)
+						}
+						
+						
+						<div>{props.data && !isNaN(props.data.investedBnBAFIT) && props.data.investedBnBAFIT > 0 ? ( <div className="claimable claimable-hidden">Invested BNB {props.data.investedBnBAFIT}</div>):(<span />)}</div>
 						<div>{props.data && !isNaN(props.data.claimableAFIT) && props.data.claimableAFIT > 0 ? ( 
 							<div>
-								<div className="u-flex-around invest-btn u-responsive-after">Claimable AFIT {props.data.claimableAFIT}</div>
+								<div className="claimable claimable-hidden">Claimable AFIT {props.data.claimableAFIT}</div>
 								<div>
 									{props.data.fundsClaimableAFIT && ! props.data.claimedAFIT ? (
 										<div className="u-flex-around invest-btn u-btn" onClick={() => {claimAFITRewards()}}>CLAIM AFIT</div>
@@ -117,7 +199,7 @@ export default function Landing(props) {
 							</div>
 						):(<span />)}</div>
 						
-						<div>{props.data && props.data.fundsClaimableAFIT && props.data.claimedAFIT ? (<div className="u-flex-around invest-btn u-responsive-after">AFIT Claimed!</div>):(<span />)}</div>
+						<div>{props.data && props.data.fundsClaimableAFIT && props.data.claimedAFIT ? (<div className="claimable claimable-hidden">AFIT Claimed!</div>):(<span />)}</div>
 						
                     </div>
                 </Fade>
@@ -125,12 +207,35 @@ export default function Landing(props) {
             <div id="buy" className="u-invest-items u-responsive-before" style={{ marginTop: "0" }}>
                 <Zoom>
                     <div className="u-invest-item">
-                        <div className="u-flex-around invest-btn u-btn">BUY AFITX</div>
+						<div className="u-flex-around invest-btn u-btn" onClick={() => {buyAfitxOn = true; }}>BUY AFITX </div>
 						
-						<div>{props.data && !isNaN(props.data.investedBnBAFITX) && props.data.investedBnBAFITX > 0 ? ( <div className="u-flex-around invest-btn">Invested BNB {props.data.investedBnBAFITX}</div>):(<span />)}</div>
+						{buyAfitxOn?
+							(
+							<div>
+							<label className="u-flex-around invest-btn u-btn">
+							BNB:
+							<input type="number" step="0.001" className="actifit-input" min={props.data.minBnBAFITX} max={props.data.maxBnBAFITX} onChange={(evt) => { 
+								if (evt.target.value < 0) evt.target.value = props.data.minBnBAFITX; 
+								if (evt.target.value > props.data.maxBnBAFITX) evt.target.value = props.data.maxBnBAFITX; 
+								bnbAfitx = evt.target.value; 
+								
+								}} 
+								
+							onBlur={(evt) => {
+								if (evt.target.value < props.data.minBnBAFITX) evt.target.value = props.data.minBnBAFITX; 
+								if (evt.target.value > props.data.maxBnBAFITX) evt.target.value = props.data.maxBnBAFITX; 
+								bnbAfitx = evt.target.value; 
+							}}	
+							/></label>
+							<div className="u-flex-around invest-btn u-btn" onClick={() => {buyAfitxConfirm(props)}}>Proceed</div>
+							</div>
+							):(<span></span>)
+						}
+						
+						<div>{props.data && !isNaN(props.data.investedBnBAFITX) && props.data.investedBnBAFITX > 0 ? ( <div className="claimable">Invested BNB {props.data.investedBnBAFITX}</div>):(<span />)}</div>
 						<div>{props.data && !isNaN(props.data.claimableAFITX) && props.data.claimableAFITX > 0 ? ( 
 							<div>
-								<div className="u-flex-around invest-btn">Claimable AFITX {props.data.claimableAFITX}</div>
+								<div className="claimable">Claimable AFITX {props.data.claimableAFITX}</div>
 								<div>
 									{props.data.fundsClaimableAFITX && ! props.data.claimedAFITX ? (
 										<div className="u-flex-around invest-btn u-btn" onClick={() => {claimAFITXRewards()}}>CLAIM AFITX</div>
@@ -139,17 +244,42 @@ export default function Landing(props) {
 								</div>
 							</div>
 						):(<span />)}</div>
-						<div>{props.data && props.data.fundsClaimableAFITX && props.data.claimedAFITX ? (<div className="u-flex-around invest-btn">AFITX Claimed!</div>):(<span />)}</div>
+						<div>{props.data && props.data.fundsClaimableAFITX && props.data.claimedAFITX ? (<div className="claimable">AFITX Claimed!</div>):(<span />)}</div>
                     </div>
 					
                 </Zoom>
                 <Zoom>
                     <div className="u-invest-item">
-                        <div className="u-flex-around invest-btn u-btn">BUY AFIT</div>
-						<div>{props.data && !isNaN(props.data.investedBnBAFIT) && props.data.investedBnBAFIT > 0 ? ( <div className="u-flex-around invest-btn">Invested BNB {props.data.investedBnBAFIT}</div>):(<span />)}</div>
+                        <div className="u-flex-around invest-btn u-btn" onClick={() => {buyAfitOn = true; }}>BUY AFIT </div>
+						
+						{buyAfitOn?
+							(
+							<div >
+								<label className="u-flex-around invest-btn u-btn">
+								BNB:
+								<input type="number" step="0.001" className="actifit-input" min={props.data.minBnBAFIT} max={props.data.maxBnBAFIT} onChange={(evt) => { 
+									if (evt.target.value < 0) evt.target.value = props.data.minBnBAFIT; 
+									if (evt.target.value > props.data.maxBnBAFIT) evt.target.value = props.data.maxBnBAFIT; 
+									bnbAfit = evt.target.value; 
+									
+									}} 
+									
+								onBlur={(evt) => {
+									if (evt.target.value < props.data.minBnBAFIT) evt.target.value = props.data.minBnBAFIT; 
+									if (evt.target.value > props.data.maxBnBAFIT) evt.target.value = props.data.maxBnBAFIT; 
+									bnbAfit = evt.target.value; 
+								}}	
+								/></label>
+								<div className="u-flex-around invest-btn u-btn" onClick={() => {buyAfitConfirm(props)}}>Proceed</div>
+							</div>
+							):(<span></span>)
+						}
+						
+						
+						<div>{props.data && !isNaN(props.data.investedBnBAFIT) && props.data.investedBnBAFIT > 0 ? ( <div className="claimable">Invested BNB {props.data.investedBnBAFIT}</div>):(<span />)}</div>
 						<div>{props.data && !isNaN(props.data.claimableAFIT) && props.data.claimableAFIT > 0 ? ( 
 							<div>
-								<div className="u-flex-around invest-btn">Claimable AFIT {props.data.claimableAFIT}</div>
+								<div className="claimable">Claimable AFIT {props.data.claimableAFIT}</div>
 								<div>
 									{props.data.fundsClaimableAFIT && ! props.data.claimedAFIT ? (
 										<div className="u-flex-around invest-btn u-btn u-responsive-after" onClick={() => {claimAFITRewards()}}>CLAIM AFIT</div>
@@ -159,7 +289,7 @@ export default function Landing(props) {
 							</div>
 						):(<span />)}</div>
 						
-						<div>{props.data && props.data.fundsClaimableAFIT && props.data.claimedAFIT ? (<div className="u-flex-around invest-btn">AFIT Claimed!</div>):(<span />)}</div>
+						<div>{props.data && props.data.fundsClaimableAFIT && props.data.claimedAFIT ? (<div className="claimable">AFIT Claimed!</div>):(<span />)}</div>
 						
                     </div>
 					
