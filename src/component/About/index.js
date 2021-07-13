fa58ec8d521408f68ba83036b986874a2b2f441d
Mohammad <mcfarhat@gmail.com>
2021-07-13 21:56:18 +0300

    minor wording changes

--- src/component/About/index.js
+++ src/component/About/index.js
@@ -12,7 +12,7 @@ export default function Landing() {
             <Fade left>
                 <div className="u-about-order">
                     <div className="u-about-title">About Actifit</div>
-                    <div className="u-about-desc">Actifit is a health-focused social project, that promotes healthier lifestyle by rewarding its users for auto-tracking their day to day fitness activities. Strongly established and running for 3 years now on Steem and Hive blockchains, with 5,000 active users and 300,000 tracked activity reports.
+                    <div className="u-about-desc">Actifit is a health-focused social project, that promotes healthier lifestyle by rewarding its users for auto-tracking their day to day fitness activities. Strongly established and running for 3 years now on Hive & Steem blockchains, with 5,000 active users and 300,000 tracked activity reports.
                         <br></br><br></br>
                         Using Actifit mobile app on Android / iOS allows you to earn token rewards via auto-tracking your everyday activity. Rewards include: AFIT tokens (Actifit’s own token), HIVE, STEEM and SPORTS token rewards via upvotes on supported blockchains and partner communities.
                         <br></br><br></br>
